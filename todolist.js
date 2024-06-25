eac976d19cb49626828373c4537060ce6fb3eefc
syedhamail01 <hamailsyed139@gmail.com>
2024-06-25 14:03:09 +0500

    first commit

---
+++ todolist.js
@@ -0,0 +1,58 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = ["Burger", "Pizza"];
+async function createTodo(todos) {
+    do {
+        let person = await inquirer.prompt({
+            type: "list",
+            message: "select an operation",
+            name: "select",
+            choices: ["Add", "Update", "View", "Delete", "Exit"]
+        });
+        if (person.select == "Add") {
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "Add item in the list",
+                name: "todo",
+            });
+            todos.push(addTodo.todo);
+            todos.forEach(todo => console.log(todo));
+        }
+        if (person.select == "Update") {
+            let updateTodo = await inquirer.prompt({
+                type: "list",
+                message: "select an item to update",
+                name: "todo",
+                choices: todos.map(item => item)
+            });
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "Add item in the list",
+                name: "todo",
+            });
+            let newTodo = todos.filter(val => val !== updateTodo.todo);
+            todos = [...newTodo, addTodo.todo];
+            todos.forEach(todo => console.log(todo));
+        }
+        if (person.select == "View") {
+            console.log("*** TO DO LIST ***");
+            todos.forEach(todo => console.log(todo));
+        }
+        if (person.select == "Delete") {
+            let deleteTodo = await inquirer.prompt({
+                type: "list",
+                message: "select an item to delete",
+                name: "todo",
+                choices: todos.map(item => item)
+            });
+            let newTodo = todos.filter(val => val !== deleteTodo.todo);
+            todos = [...newTodo];
+            todos.forEach(todo => console.log(todo));
+        }
+        if (person.select == "Exit") {
+            console.log("Good Bye");
+            break;
+        }
+    } while (true);
+}
+createTodo(todos);
